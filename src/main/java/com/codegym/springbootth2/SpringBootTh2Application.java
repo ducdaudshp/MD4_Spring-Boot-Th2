8ee02a6e3c7243d414fd4e311e3bad4ec53dafd1
ducdaudshp <ducdaudshp@gmail.com>
2022-05-05 11:04:41 +0700

    spring boot thuc hanh 2

---
+++ src/main/java/com/codegym/springbootth2/SpringBootTh2Application.java
@@ -0,0 +1,13 @@
+package com.codegym.springbootth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTh2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTh2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/springbootth2/SpringBootTh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.springbootth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTh2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
